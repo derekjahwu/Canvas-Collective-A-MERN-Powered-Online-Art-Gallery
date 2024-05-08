065c6b3a8cc01b5189f08139ffb271c149e75bdf
Derek Wu <Derek.Wu@glidewelldental.com>
2024-05-08 09:40:14 -0700

    simplified GalleryArea.js map function

--- src/components/GalleryArea.js
+++ src/components/GalleryArea.js
@@ -5,19 +5,13 @@ import Row from 'react-bootstrap/Row';
 import axios from 'axios';
 import Spinner from 'react-bootstrap/Spinner';
 
-//Render link for express server: https://express-paintings-server.onrender.com/api/paintings
-
 const GalleryArea = () => {
     const [collection, setCollection] = useState([])
     const [show, setShow] = useState("block")
 
-    let loadPaintings = (data) => {
-        return data.map(createPainting)
-    };
-
-    let createPainting = (painting) => {
-        return <PaintingCard  class="card" title={painting.title} artist={painting.artist} date={painting.date} link={painting.link} key={painting._id} _id={painting._id} />
-    };
+    let paintings = collection.map(painting => 
+        <PaintingCard  class="card" title={painting.title} artist={painting.artist} date={painting.date} link={painting.link} key={painting._id} _id={painting._id} />
+    )
 
     const getPaintings = async () => {
         await axios.get('https://express-paintings-server.onrender.com/api/paintings')
@@ -39,7 +33,7 @@ const GalleryArea = () => {
                 <Spinner style={{display:show}} animation="border"  />
             </Container>
             <Row>
-                {loadPaintings(collection)}
+                {paintings}
             </Row>
         </Container>
     )
